3e3b2998fffccb6aff6881afbb961ee028fb0dbc
Cara Steffen <cara@treadstonedesign.com>
2019-11-11 20:51:58 -0700

    jwt authorization

---
+++ src/middleware/basic-auth.js
@@ -0,0 +1,32 @@
+const AuthService = require("../auth/auth-service");
+
+function requireAuth(req, res, next) {
+  const authToken = req.get("Authorization") || "";
+
+  let basicToken;
+
+  if (!authToken.toLowerCase().startsWith("basic ")) {
+    return res.status(401).json({ error: "Missing basic token" });
+  } else {
+    basicToken = authToken.slice("basic ".length, authToken.length);
+  }
+
+  const [tokenUserName, tokenPassword] = AuthService.parseBasicToken(
+    basicToken
+  );
+
+  if (!tokenUserName || !tokenPassword) {
+    return res.status(401).json({ error: "Unauthorized request" });
+  }
+
+  AuthService.getUserWithUserName(req.app.get("db"), tokenUserName)
+    .then(user => {
+      if (!user || user.password !== tokenPassword) {
+        return res.status(401).json({ error: "Unauthorized request" });
+      }
+      next();
+    })
+    .catch(next);
+}
+
+module.exports = { requireAuth };
